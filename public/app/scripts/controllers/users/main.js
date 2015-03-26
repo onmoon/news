733cf76f7836b49c4e23160b360db5d29cd00720
Ivan Dolgov <idolgov@fbs.com>
2015-03-26 18:27:27 +0400

    select active group

--- public/app/scripts/controllers/users/main.js
+++ public/app/scripts/controllers/users/main.js
@@ -3,9 +3,17 @@ app
 	.controller('usersMainCtrl', ['$scope','groupsResolve', 'usersResolve', '$modal', 'Restangular',
 	function ( $scope, groupsResolve, usersResolve, $modal, Restangular) {
 
+
+		$scope.active = {
+			group : -1
+		};
 		$scope.groups = groupsResolve;
-		$scope.users = usersResolve;
+		$scope.users = usersResolve.plain();
 		$scope.user = _.first($scope.users);
+
+		$scope.selectGroup = function (group) {
+			$scope.active.group = group.id;
+		}
 
 
 		$scope.addGroup = function (group) {
