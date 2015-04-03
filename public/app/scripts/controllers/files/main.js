fbbaa77d26da1923bc585e91c1ae4ca99cf7cfa3
Ivan Dolgov <idolgov@fbs.com>
2015-04-03 18:53:06 +0400

    add files for upload

--- public/app/scripts/controllers/files/main.js
+++ public/app/scripts/controllers/files/main.js
@@ -1,6 +1,15 @@
 'use strict';
 app
-    .controller('filesMainCtrl', ['$scope', 'filesCollection',
-    function ( $scope, filesCollection) {
+    .controller('filesMainCtrl', ['$scope', '$modal', 'filesCollection',
+    function ( $scope, $modal, filesCollection) {
     	$scope.files = filesCollection;
+
+    	$scope.openUpload = function (group) {
+			$modal({
+				scope: $scope,
+				template: 'views/modals/files/upload.html',
+				placement : 'center',
+				show: true
+			});
+		};
     }]);
